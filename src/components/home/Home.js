e0f9649d648f222189b12f5fa6c5d03e3cbd38dd
Omkar <omkar.a.g.78@gmail.com>
2022-04-06 09:20:56 +0530

    Removed Home image

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../css/home.css';
-import myImage from '../images/my-image.png';
+// import myImage from '../images/my-image.png';
 import { Typewriter } from 'react-simple-typewriter';
 
 const Home = () => {
@@ -11,7 +11,7 @@ const Home = () => {
 		<>
 			<div className="container my-5">
 				<div className="row home-main">
-					<div className="col-lg-8 col-md-12 home-cols">
+					<div className="col-lg-8 col-md-12 home-cols mx-auto">
 						<div className="my-name" style={{ height: '5rem', width: 'fit-content' }}>
 							<h1>
 								<Typewriter
@@ -21,7 +21,7 @@ const Home = () => {
 									loop={0} />
 							</h1>
 						</div>
-						<div className=" card-shadow border-8">
+						<div className=" card-shadow border-8 p-4">
 							<div className="my-field">
 								<h1>FULL STACK WEB DEVELOPER INTERN</h1>
 							</div>
@@ -31,9 +31,9 @@ const Home = () => {
 						</div>
 					</div>
 
-					<div className="col-lg-4 col-md-12 home-cols">
+					{/* <div className="col-lg-4 col-md-12 home-cols">
 						<img className='myImage card-shadow rounded-circle' src={myImage} alt="" />
-					</div>
+					</div> */}
 				</div>
 			</div>
 

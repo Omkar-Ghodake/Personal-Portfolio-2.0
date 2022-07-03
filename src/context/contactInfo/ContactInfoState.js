15f1b6de59438d4a9714148fdba03c9199afd2f9
Omkar <omkar.a.g.78@gmail.com>
2022-07-03 16:10:37 +0530

    Updated twitter profile link

--- src/context/contactInfo/ContactInfoState.js
+++ src/context/contactInfo/ContactInfoState.js
@@ -8,7 +8,7 @@ const ContactInfoState = (props) => {
 		{ icon: 'fa-solid fa-envelope', name: 'Email', title: 'omkar.a.g.78@gmail.com', mailId: 'omkar.a.g.78@gmail.com' },
 		{ icon: 'fa-brands fa-github', name: 'GitHub', title: 'Omkar-Ghodake', url: 'https://github.com/Omkar-Ghodake' },
 		{ icon: 'fa-brands fa-linkedin', name: 'LinkedIn', title: 'omkar-ghodake', url: 'https://www.linkedin.com/in/omkar-ghodake/' },
-		{ icon: 'fa-brands fa-twitter', name: 'Twitter', title: 'Omkar Ghodake', url: 'https://twitter.com/omkar_g_1831' },
+		{ icon: 'fa-brands fa-twitter', name: 'Twitter', title: 'Omkar Ghodake', url: 'https://twitter.com/omkar__ghodake' },
 		{ icon: 'fa-brands fa-telegram', name: 'Telegram', title: 'Omkar Ghodake', url: 'https://t.me/omkar_ghodake' },
 		{ icon: 'fa-brands fa-instagram', name: 'Instagram', title: 'omkar_1831_', url: 'https://www.instagram.com/omkar_1831_/' },
 		{ icon: 'fa-brands fa-facebook', name: 'Facebook', title: 'Omkar Ghodake', url: 'https://www.facebook.com/omkar.ghodake.92/' }

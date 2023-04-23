6b554cb0daf2b42070b7746bfb50a42e57095f22
test-bot-69 <testbot.1831@gmail.com>
2023-04-23 21:26:35 +0530

    changed username

--- src/context/contactInfo/ContactInfoState.js
+++ src/context/contactInfo/ContactInfoState.js
@@ -8,9 +8,9 @@ const ContactInfoState = (props) => {
 		{ icon: 'fa-solid fa-envelope', name: 'Email', title: 'omkar.a.g.78@gmail.com', mailId: 'omkar.a.g.78@gmail.com' },
 		{ icon: 'fa-brands fa-github', name: 'GitHub', title: 'Omkar-Ghodake', url: 'https://github.com/Omkar-Ghodake' },
 		{ icon: 'fa-brands fa-linkedin', name: 'LinkedIn', title: 'omkar-ghodake', url: 'https://www.linkedin.com/in/omkar-ghodake/' },
-		{ icon: 'fa-brands fa-twitter', name: 'Twitter', title: 'Omkar Ghodake', url: 'https://twitter.com/omkar__ghodake' },
-		{ icon: 'fa-brands fa-telegram', name: 'Telegram', title: 'Omkar Ghodake', url: 'https://t.me/omkar_ghodake' },
-		{ icon: 'fa-brands fa-instagram', name: 'Instagram', title: 'omkar_1831_', url: 'https://www.instagram.com/_omkar.ghodake_/' },
+		{ icon: 'fa-brands fa-twitter', name: 'Twitter', title: 'omkar__ghodake', url: 'https://twitter.com/omkar__ghodake' },
+		{ icon: 'fa-brands fa-telegram', name: 'Telegram', title: 'omkar_ghodake', url: 'https://t.me/omkar_ghodake' },
+		{ icon: 'fa-brands fa-instagram', name: 'Instagram', title: '_omkar.ghodake_', url: 'https://www.instagram.com/_omkar.ghodake_/' },
 		{ icon: 'fa-brands fa-facebook', name: 'Facebook', title: 'Omkar Ghodake', url: 'https://www.facebook.com/omkar.ghodake.92/' }
 	]);
 
